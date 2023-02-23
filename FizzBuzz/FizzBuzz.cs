cad83a68a4f954f4f14d6906093dbbda48f45113
bcicada <b.cicada@protonmail.com>
2023-02-23 03:37:44 +0100

    Init, adding FizzBuzz and TestFizzBuzz.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,36 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public string Generate(int n)
+        {
+            if (n < 1 || n > 150)
+            {
+                throw new ArgumentException("Le nombre doit être compris entre 15 et 150");
+            }
+
+            string result = "";
+            for (int i = 1; i <= n; i++)
+            {
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    result += "FizzBuzz ";
+                }
+                else if (i % 3 == 0)
+                {
+                    result += "Fizz ";
+                }
+                else if (i % 5 == 0)
+                {
+                    result += "Buzz ";
+                }
+                else
+                {
+                    result += i.ToString() + " ";
+                }
+            }
+
+            return result.TrimEnd();
+        }
+    }
+}
---
+++ TestFizzBuzz/TestFizzBuzz.cs
@@ -0,0 +1,21 @@
+namespace TestFizzBuzz
+{
+    public class TestFizzBuzz
+    {
+        [Theory]
+        [InlineData(15, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz")]
+        [InlineData(20, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz")]
+        [InlineData(30, "1 2 Fizz 4 Buzz Fizz 7 8 Fizz Buzz 11 Fizz 13 14 FizzBuzz 16 17 Fizz 19 Buzz Fizz 22 23 Fizz Buzz 26 Fizz 28 29 FizzBuzz")]
+        public void Generate_ReturnsCorrectString(int n, string expected)
+        {
+            // Arrange
+            var generator = new FizzBuzz.FizzBuzz();
+
+            // Act
+            string actual = generator.Generate(n);
+
+            // Assert
+            Assert.Equal(expected, actual);
+        }
+    }
+}
