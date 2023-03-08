23ce1ea3d74ff2bf2a76badc54199b6196928d45
bcicada <b.cicada@protonmail.com>
2023-03-08 23:56:24 +0100

    Refactor ILoanCost and adding LoanCost

---
+++ CreditImmobilier/Loan/LoanCost/ILoanCost.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditImmobilier.Loan.LoanCost
+{
+    public interface ILoanCost
+    {
+        double CalculateMonthlyLoanCost();
+        double CalculateTotalLoanCost();
+    }
+}
---
+++ CreditImmobilier/Loan/LoanCost/LoanCost.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditImmobilier.Loan.LoanCost
+{
+    public class LoanCost : ILoanCost
+    {
+    }
+
+    public double CalculateMonthlyLoanCost()
+    {
+
+    }
+
+    public double CalculateTotalLoanCost()
+    {
+
+    }
+}
