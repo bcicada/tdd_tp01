778d00c9e15262da52971d7cf250daf632c08596
bcicada <b.cicada@protonmail.com>
2023-03-08 23:41:07 +0100

    Adding LoanAmount and LoanTermInMonths class's.

---
+++ CreditImmobilier/LoanAmount.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditImmobilier
+{
+    public class LoanAmount
+    {
+        private double _amount { get; }
+
+        public LoanAmount(double amount)
+        {
+            _amount = amount < 50000 ? throw new ArgumentOutOfRangeException(nameof(amount), "The amount borrowed must be greater than 50000.") : amount;
+        }
+    }
+}
---
+++ CreditImmobilier/LoanTermInMonths.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreditImmobilier
+{
+    public class LoanTermInMonths
+    {
+        private int _loanTermInMonths { get; }
+
+        public LoanTermInMonths(int loanTermInMonths) 
+        {
+            if (loanTermInMonths < 108)
+                throw new ArgumentOutOfRangeException(nameof(loanTermInMonths), "The number of monthly payments must be greater than 108 months.");
+            else if (loanTermInMonths > 300)
+                throw new ArgumentOutOfRangeException(nameof(loanTermInMonths), "The number of monthly payments must be less than 300 months.");
+
+            _loanTermInMonths = loanTermInMonths;
+        }
+    }
+}
